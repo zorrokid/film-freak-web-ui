61ea73527fa3893198d85d53121187c53ba03eaa
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-03-15 23:43:09 +0200

    read tab separated values

--- src/routes/import/ImportForm.tsx
+++ src/routes/import/ImportForm.tsx
@@ -1,24 +1,45 @@
 import React from "react";
 
 const MAX_FILE_SIZE_BYTES = 10000;
-const FILE_MIME_TYPE_CSV = 'text/csv';
+const FILE_MIME_TYPE_CSV = 'text/tab-separated-values';
 
 export const ImportForm: React.FC = () => {
     const processFile = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (!event.target.files) return;
+        if (!event.target.files) {
+            console.log('no files');
+            return;
+        }
+
         if (event.target.files.length > 0) {
             const file: File = event.target.files[0];
-            if (file.size < MAX_FILE_SIZE_BYTES && file.type === FILE_MIME_TYPE_CSV) {
-                const reader: FileReader = new FileReader();
-                reader.readAsText(file);
-                reader.addEventListener("load", (event: ProgressEvent<FileReader>) => {
-                    console.log(reader.result);
-                });
+            console.log('start reading file', file);
+            if (file.size < MAX_FILE_SIZE_BYTES) {
+                console.log('too large file');
+                return;
             }
+            if (file.type !== FILE_MIME_TYPE_CSV) {
+                console.log('wrong file type');
+                return;
+            }
+            const reader: FileReader = new FileReader();
+            reader.readAsText(file);
+            reader.addEventListener("load", (event: ProgressEvent<FileReader>) => {
+                const resultString = reader.result as string;
+                if (!resultString) return;
+                const rows = resultString.split('\n');
+                rows.forEach((value: string, index: number) => {
+                    if (index === 0) {
+                        // skip header
+                        return;
+                    }
+                    const fields = value.split('\t');
+                    console.log(fields);
+                });
+            });
         }
     }
 
     return <>
-        <input type="file" onChange={processFile} accept=".csv" />
+        <input type="file" onChange={processFile} accept=".tsv" />
     </>;
 }
