8e9a5ac47273261fdd3b817969d01ffdff3073eb
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-03-27 21:21:23 +0300

    remove debugger

--- src/routes/import/importApiService.ts
+++ src/routes/import/importApiService.ts
@@ -9,7 +9,6 @@ export interface ImportResponse {
 export const doImportAsync = async (importRows: ImportItem[]): Promise<ImportResponse> => {
     const url = `${process.env.REACT_APP_API_URL}/import`;
     const response = await axios.post<ImportResponse>(url, importRows);
-    debugger;
     const status = response.status;
     if (status !== 200) throw new Error(`Import request failed with status ${status}`);
     return response.data;
