509c41ab6e4eafc68957f03d9b368749b1e6847e
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-03-27 23:37:36 +0300

    Use status instead of use effect to load releases

--- src/routes/releases/ReleasesList.tsx
+++ src/routes/releases/ReleasesList.tsx
@@ -1,19 +1,22 @@
-import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../app/store";
-import { getReleasesAsync } from "./releasesSlice";
+import { getReleasesAsync, ReleasesStatus } from "./releasesSlice";
 
 export const ReleasesList: React.FC = () => {
     const dispatch = useDispatch<AppDispatch>();
     const releases = useSelector((state: RootState) => state.releases.releases);
-    useEffect(() => {
+    const status = useSelector((state: RootState) => state.releases.status);
+    if (releases.length === 0 && status === ReleasesStatus.initial) {
         dispatch(getReleasesAsync());
-    }, []);
-    return <ul>
-        {
-            releases.map(r => <li key={r.id}>{r.title}</li>)
-        }
+    }
 
-    </ul>;
+    return (
+        <ul>
+            {
+                releases.map(r => <li key={r.id}>{r.title}</li>)
+            }
+        </ul>
+    );
 
 }
+
